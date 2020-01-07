b7d23621bbce8c14943be8348238ce1ed2b2272f
Nathan Birus <nathan.birus@nih.gov>
2020-01-07 11:44:07 -0600

    test

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
 class Message(Resource):
-    def get(self):
-        return {'hello': 'world'}
+    def post(self):
 
-api.add_resource(Message, '/post')
+        # get data from body
+        data = request.get_data()
+
+        # email message to me
+        email_message(data)
+
+        return {'status': 200}
+
+api.add_resource(Message, '/')
+
+def email_message(msg):
+  print(msg)
 
 if __name__ == '__main__':
     app.run(debug=True)
